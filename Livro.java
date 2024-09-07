7ee55840e5712c42fecd60ae65bd5e341c533fec
NayanaRoberta <nayana_roberta@hotmail.com>
2024-09-07 17:46:43 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,17 @@
+public class Livro {
+	private String titulo;
+	private String autor;
+	private String editora;
+	private int preco;
+	private String status;
+	
+	
+	public Livro(String titulo, String autor, String editora, int preco, String status) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.editora = editora;
+		this.preco = preco;
+		this. status = status
+
+	}
+}
