cfcba1d94628c2e5da29c74c292453b10eeb3202
NayanaRoberta <nayana_roberta@hotmail.com>
2024-09-08 11:29:07 -0300

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -2,11 +2,11 @@ public class Livro {
 	private String titulo;
 	private String autor;
 	private String editora;
-	private double preco;
+	private int preco;
 	private String status;
 	
 	
-	public Livro(String titulo, String autor, String editora, int preco, String status) {
+public Livro(String titulo, String autor, String editora, int preco, String status) {
 		this.titulo = titulo;
 		this.autor = autor;
 		this.editora = editora;
