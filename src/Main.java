43d595d50ea7941ec3c945f02a9720715e548356
Flávia Amaro <flaviaamaro1605@gmail.com>
2024-09-08 19:50:28 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,13 @@ import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Main { //classe para testar o código;
     public static void main(String[] args) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Loan loan = new Loan(1, 50.00f,1, 2345);
-        String returnDate = "20/09/2024";
-        Date date = sdf.parse(returnDate);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); //define o formato da data;
+        Loan loan = new Loan(1, 50.00f,1, 2345); //cria um novo objeto do tipo Loan(Empréstimo);
         loan.dateDelivery();
         loan.receipt();
-        loan.devolucao(date);
+        loan.devolucao(sdf.parse("20/09/2024")); //chama a função devolução passando uma data do tipo string como parâmetro e a converte em milissegundos, para que possa ser utilizada na operação matemática da função.
     }
 
 }
