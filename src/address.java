0ef4ff6ae01f0990d9e4e11a920709327bc5edf7
Vitor Rafael <contatorafaelth@gmail.com>
2024-09-10 20:21:37 -0300

    Update address.java

--- src/address.java
+++ src/address.java
@@ -6,11 +6,14 @@ public class address {
     public String Cidade;
     public String Estado;
 
+    // Construtor responsável por inicializar os atributos;
+
     public address(String CEP, String Rua, int Numero, String Bairro, String Cidade, String Estado) {
 
-        
+        // Verifica se tem 8 dígitos e se contém apenas números;
         if (CEP.length() != 8 || !CEP.matches("\\d+")) {
             throw new IllegalArgumentException(" O CEP deve conter 9 dígitos. Exemplo: 10101101");
+            // Lança uma exceção caso o CEP seja inválido;
         }
 
         this.CEP = CEP;
@@ -21,6 +24,8 @@ public class address {
         this.Estado = Estado;
     }
 
+
+    // Imprime o endereço de modo formatado;
     public void exibirEndereco() {
             System.out.println("Rua " + Rua + ", " + Numero + " - " + Bairro + " (" + Cidade + "/" + Estado + ")");
     }
