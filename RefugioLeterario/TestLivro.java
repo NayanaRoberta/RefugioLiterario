36bdd5cf6bbc0fa7e4155df0d153f428489d091f
Vitor Rafael <contatorafaelth@gmail.com>
2024-09-10 20:29:57 -0300

    Uptade
    Organização dos arquivos
    
    Co-Authored-By: NayanaRoberta <169853314+NayanaRoberta@users.noreply.github.com>

--- RefugioLeterario/TestLivro.java
+++
@@ -1,7 +0,0 @@
-//public class TestLivro {
-    //public static void main(String[] args) {
-       // Livro livro1 = new Livro("After", "Anna Tood", "Paralela", 48.50, true);
-
-    //System.out.println(livro1.toString());
-    //}
-//}
--- src/Livro.java
+++ src/Livro.java
@@ -1,50 +1,61 @@
 public class Livro {
-	private String titulo;
-	private String autor;
-	private String editora;
-	private int preco;
-	private String status;
+    private String nome;
+    private String autor;
+    private String editora;
+    private int preco;
+    private boolean status; 
+    // true = disponível. false = emprestado
 
-// construtor
-	
-public Livro(String titulo, String autor, String editora, int preco, String status){
-		this.titulo = titulo;
-		this.autor = autor;
-		this.editora = editora;
-		this.preco = preco;
-		this. status = status;
+    // construtor da classe
+public Livro(String nome, String autor, String editora, int preco, boolean status) {
+    this.nome = nome;
+    this.autor = autor;
+    this.editora = editora;
+    this.preco = preco;
+    this.status = status;
 
 }
 
-//metodo 
+// getters
+public String getNome() {
+    return nome;
 
-public String getTitulo() {
-	return titulo;
 }
 
 public String getAutor() {
-	return autor;
+    return autor;
 }
 
-public String getEditora() {
-	return editora;
-
+public int getPreco() {
+    return preco;
 }
 
-public int getPreco() }
-	return preco;
+public boolean isDisponivel() {
+    return status;
 }
 
+// setters
 
-public String getStatus() {
-	return status;
+public void setNome(String nome) {
+    this.nome = nome;
 }
 
-//metodo toString para mostrar informaçãos do livro
-@Override
-	
+public void SetAutor(String autor) {
+    this.autor = editora;
+}
+
+public void setPreco(int preco) {
+    this.preco = preco;
+}
+
+public void setStatus(boolean  status) {
+    this.status = status;
+}
+
+// tosTring
 public String toString() {
-	return "Titulo: " + titulo + ", Autor: " + autor + ", Editora: " + editora + ", Preco: " + preco + ", Status: " + status +";
+    String disponibilidade = (status) ? "Disponível" : "Emprestado";
+    return "Nome: " + nome + ", Autor:  " + autor + ", Editora: " + editora + ", Preço: R$" + preco + ", Status: " + disponibilidade;
 }
 
 }
