0eaf1b9c3e7b99fac7043d245b69c2cbf77e3d20
NayanaRoberta <nayana_roberta@hotmail.com>
2024-09-10 21:19:01 -0300

    Update Livro.java

--- src/Livro.java
+++ src/Livro.java
@@ -1,3 +1,4 @@
+//armazenar informações sobre o livro
 public class Livro {
     private String nome;
     private String autor;
@@ -6,7 +7,7 @@ public class Livro {
     private boolean status; 
     // true = disponível. false = emprestado
 
-    // construtor da classe
+    // construtor da classe = criar um novo livro
 public Livro(String nome, String autor, String editora, int preco, boolean status) {
     this.nome = nome;
     this.autor = autor;
@@ -16,43 +17,44 @@ public Livro(String nome, String autor, String editora, int preco, boolean statu
 
 }
 
-// getters
-public String getNome() {
+// getters = pegar as informações dos atributos
+//obter nome para o livro
+    public String getNome() {
     return nome;
 
 }
-
+//obter autor para o livro
 public String getAutor() {
     return autor;
 }
-
+// obter valor para a multa = atraso livro
 public int getPreco() {
     return preco;
 }
-
+//saber se está disponivel ou não disponive para o livro
 public boolean isDisponivel() {
     return status;
 }
 
-// setters
-
+// setters = mudar os atributos 
+// novo nome para o livro
 public void setNome(String nome) {
     this.nome = nome;
 }
-
+//novo autor 
 public void SetAutor(String autor) {
     this.autor = editora;
 }
-
+//novo valor para a multa
 public void setPreco(int preco) {
     this.preco = preco;
 }
-
+//definor noovo status  
 public void setStatus(boolean  status) {
     this.status = status;
 }
 
-// tosTring
+// tosTring = retorna uma representação em texto das informações do livro
 public String toString() {
     String disponibilidade = (status) ? "Disponível" : "Emprestado";
     return "Nome: " + nome + ", Autor:  " + autor + ", Editora: " + editora + ", Preço: R$" + preco + ", Status: " + disponibilidade;
